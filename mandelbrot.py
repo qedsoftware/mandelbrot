a2b7eab251c7248d813de6d182ecf9d4bee50fce
William Wu <w@qed.ai>
2018-08-13 01:55:25 +0300

    separated scalar and vector implementations as subclasses

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,12 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from abc import ABC, abstractmethod
 
 
 class Mandelbrot:
+
     def __init__(self, x_min, x_max, x_len, y_min, y_max, y_len, num_iters):
         """
-        :param grid_len: Number of points to be used in one dimension of the
-            meshgrid corresponding to [-2,2] x [-2,2] in the complex plane.
         :param num_iters: Number of iterations of the z <- z^2 + c map.
         """
         self.x_min = x_min
@@ -19,7 +19,18 @@ class Mandelbrot:
         self.x = np.linspace(x_min, x_max, x_len)
         self.y = np.linspace(y_min, y_max, y_len)
 
-    def is_mandelbrot_scalar(self, c):
+    @abstractmethod
+    def is_mandelbrot(self, c):
+        pass
+
+    @abstractmethod
+    def generate(self):
+        pass
+
+
+class MandelbrotScalar(Mandelbrot):
+
+    def is_mandelbrot(self, c):
         z = 0
         for i in range(self.num_iters):
             z = z * z + c
@@ -27,17 +38,20 @@ class Mandelbrot:
                 return False
         return True
 
-    def generate_scalar(self):
+    def generate(self):
         z = np.empty((self.y_len, self.x_len))
         for j in range(self.y_len):
             for i in range(self.x_len):
                 a = self.x[i]
                 b = self.y[j]
-                z[j, i] = self.is_mandelbrot_scalar(a + b * 1j)
+                z[j, i] = self.is_mandelbrot(a + b * 1j)
         plt.contourf(self.x, self.y, z)
         plt.show()
 
-    def is_mandelbrot_vector(self, c):
+
+class MandelbrotVector(Mandelbrot):
+
+    def is_mandelbrot(self, c):
         z = np.zeros((self.y_len, self.x_len)) + np.zeros(
             (self.y_len, self.x_len)) * 1j
         not_mandelbrot = np.full(z.shape, False)
@@ -47,8 +61,8 @@ class Mandelbrot:
             z[not_mandelbrot] = 0
         return ~not_mandelbrot
 
-    def generate_vector(self):
+    def generate(self):
         xx, yy = np.meshgrid(self.x, self.y, sparse=True)
-        z = self.is_mandelbrot_vector(xx + yy * 1j)
+        z = self.is_mandelbrot(xx + yy * 1j)
         plt.contourf(self.x, self.y, z)
         plt.show()
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from mandelbrot import Mandelbrot
+from mandelbrot import Mandelbrot, MandelbrotScalar, MandelbrotVector
 import time
 
 x_min = -1.75
@@ -9,9 +9,15 @@ y_max = 1.25
 y_len = 200
 num_iters = 100
 
-m = Mandelbrot(x_min, x_max, x_len, y_min, y_max, y_len, num_iters)
+m_s = MandelbrotScalar(x_min, x_max, x_len, y_min, y_max, y_len, num_iters)
+m_v = MandelbrotVector(x_min, x_max, x_len, y_min, y_max, y_len, num_iters)
 
-t0 = time.time()
-m.generate_scalar()
-t1 = time.time()
-print(t1-t0)
+def render_and_time(m: Mandelbrot):
+    start = time.time()
+    m.generate()
+    end = time.time()
+    elapsed = end - start
+    print (f"{m.__class__.__name__}: {elapsed}")
+
+for m in [m_v]:
+    render_and_time(m)
